426ddd9227cfc9c956c51b9761eba27985a5e6b9
Nim <nim.team@bskyb.com>
2015-04-14 14:13:42 +0100

    fixed symlink syntax.

--- netstream-mvn-3.2.1.rb
+++ netstream-mvn-3.2.1.rb
@@ -5,13 +5,13 @@
 class NetstreamMvn321 < Formula
   homepage ""
   url "https://github.com/peterfich/netstream-mvn-3.2.1/archive/0.0.2.tar.gz"
-  version "0.6"
+  version "0.7"
   sha256 "351e2db2eff90aba254fd574fe5c91ff08876d81f5524d4de5475e9564c4f50f"
   
   def install
     mkdir_p '/easy'
     cp_r 'apache-maven-3.2.1', '/easy'
-    ln_s '/easy/apache-maven-3.2.1/bin/mvnDebug' HOMEBREW_PREFIX'/mvnDebug2'
+    ln_s '/easy/apache-maven-3.2.1/bin/mvnDebug', HOMEBREW_PREFIX + '/mvnDebug2'
     bin.install_symlink '/easy/apache-maven-3.2.1/bin/mvnDebug' 
   end
 end
