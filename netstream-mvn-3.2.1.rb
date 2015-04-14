5781c6c2549db34f3d5b7cdaf5d94c61407d7e27
Nim <nim.team@bskyb.com>
2015-04-14 14:00:31 +0100

    Try to link mvnDebug instead.

--- netstream-mvn-3.2.1.rb
+++ netstream-mvn-3.2.1.rb
@@ -5,12 +5,12 @@
 class NetstreamMvn321 < Formula
   homepage ""
   url "https://github.com/peterfich/netstream-mvn-3.2.1/archive/0.0.2.tar.gz"
-  version "0.3"
+  version "0.4"
   sha256 "351e2db2eff90aba254fd574fe5c91ff08876d81f5524d4de5475e9564c4f50f"
   
   def install
     mkdir_p '/easy'
     cp_r 'apache-maven-3.2.1', '/easy'
-    bin.install_symlink '/easy/apache-maven-3.2.1/bin/mvn'
+    install_symlink '/easy/apache-maven-3.2.1/bin/mvnDebug' 
   end
 end
