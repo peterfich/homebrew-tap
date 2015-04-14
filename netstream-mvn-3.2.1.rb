943d16b1757e90c43e63dc6a1c61af6497d12cc7
Nim <nim.team@bskyb.com>
2015-04-14 11:46:17 +0100

    Added mvn 3.2.1

---
+++ netstream-mvn-3.2.1.rb
@@ -0,0 +1,16 @@
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class NetstreamMvn321 < Formula
+  homepage ""
+  url "https://github.com/peterfich/netstream-mvn-3.2.1/archive/0.0.2.tar.gz"
+  version "0.1"
+  sha256 "351e2db2eff90aba254fd574fe5c91ff08876d81f5524d4de5475e9564c4f50f"
+
+  def install
+    mkdir_p '/easy'
+    cp_r 'apache-maven-3.2.1', '/easy'
+    bin.install '/easy/apache-maven-3.2.1/bin/mvn'
+  end
+end
